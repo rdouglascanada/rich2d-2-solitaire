c8ff9887f21ff29ed734fa23f1846cce97e77536
rdouglascanada <rdouglascanada@gmail.com>
2023-06-10 01:42:59 -0400

    0.1.0 Add Simple Window with title and dark green background

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from rich2d.game import Game, GameConfig
+from rich2d.models import Model
+
+window_width = 800
+window_height = 600
+
+game_config = GameConfig(window_width=window_width, window_height=window_height,
+                         window_title="Solitaire", background_colour="darkgreen")
+game_model = Model()
+snake_game = Game(model=game_model, config=game_config)
+snake_game.run()
